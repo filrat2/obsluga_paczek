7e7eab953858fcebb8ac948374ce93588db4cbd8
Filip Ratajszczak <ratajszczak.filip@gmail.com>
2022-08-09 23:27:09 +0200

    this is simpler and shorter (but still working) version of this script

--- obsluga_paczek.py
+++ obsluga_paczek.py
@@ -26,7 +26,6 @@ for i in range(1, (liczba_przedmiotow + 1)):
     waga_przedmiotu = float(input("Podaj wagę przedmiotu: "))
     if waga_przedmiotu == 0:
         print(wiadomosc0)
-        lista.append(int(999))
         break
     if waga_przedmiotu < 1 or waga_przedmiotu > 10:
         print(wiadomosc1)
@@ -43,24 +42,6 @@ for i in range(1, (liczba_przedmiotow + 1)):
         wagi_przesylek.append(sum(biezaca_przesylka))
         biezaca_przesylka = []
         biezaca_przesylka.append(waga_przedmiotu)
-
-
-# if user input item weigth == 0
-if lista[-1] == int(999):
-    lista = lista[:-1]
-    for waga in lista:
-        biezaca_przesylka.append(waga_przedmiotu)
-        if sum(biezaca_przesylka) == 20:
-            print(wiadomosc2)
-            wagi_przesylek.append(sum(biezaca_przesylka))
-            biezaca_przesylka = []
-        if sum(biezaca_przesylka) > 20:
-            print(wiadomosc2)
-            biezaca_przesylka = biezaca_przesylka[:-1]
-            wagi_przesylek.append(sum(biezaca_przesylka))
-            biezaca_przesylka = []
-            biezaca_przesylka.append(waga_przedmiotu)
-
 
 waga_ost_paczki = sum(biezaca_przesylka)
 
@@ -88,4 +69,4 @@ if sum(puste_kg) > 0:
     print(f"\nLiczba wysłanych 'pustych' kilogramów wynosi"
           f" {(len(wagi_przesylek)*20)-sum(lista)}."
           f"\nPaczka z największą liczbą wysłanych 'pustych' kilogramów to paczka"
-          f" nr {index}, \nw której wysłano {maks_puste_kg} 'pustych' kilogramów")
+          f" nr {index}, \nw której wysłano {maks_puste_kg} 'pustych' kilogramów.")
