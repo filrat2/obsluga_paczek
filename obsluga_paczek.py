e1dbeef048873003af666bf2e4d45910472b12a1
Filip Ratajszczak <ratajszczak.filip@gmail.com>
2022-08-09 23:15:48 +0200

    next (probably final) version of obsluga_paczek.py script

--- obsluga_paczek.py
+++ obsluga_paczek.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 
 # messages for user
-wiadomosc0 = """Jeśli podano element o wadze 0, program powinien zakończyć
-działanie, tak jakby maksymalna liczba paczek została osiągnięta."""
+wiadomosc0 = "\nWprowadzanie przedmiotów zakończone."
 
-wiadomosc1 = """Error! Działanie programu zakończone.
+wiadomosc1 = """\nError! Wprowadzanie przedmiotów zakończone.
 Waga przedmiotu musi mieścić się w zakresie 1.00 - 10.00 kg."""
 
 wiadomosc2 = """Górny limit wagi przesyłki osiagnięty.
@@ -21,11 +20,13 @@ biezaca_przesylka = []
 wagi_przesylek = []
 puste_kg = []
 
+
 # for loop
 for i in range(1, (liczba_przedmiotow + 1)):
     waga_przedmiotu = float(input("Podaj wagę przedmiotu: "))
     if waga_przedmiotu == 0:
         print(wiadomosc0)
+        lista.append(int(999))
         break
     if waga_przedmiotu < 1 or waga_przedmiotu > 10:
         print(wiadomosc1)
@@ -43,7 +44,24 @@ for i in range(1, (liczba_przedmiotow + 1)):
         biezaca_przesylka = []
         biezaca_przesylka.append(waga_przedmiotu)
 
-# obiekty_w_paczce = len(biezaca_przesylka)
+
+# if user input item weigth == 0
+if lista[-1] == int(999):
+    lista = lista[:-1]
+    for waga in lista:
+        biezaca_przesylka.append(waga_przedmiotu)
+        if sum(biezaca_przesylka) == 20:
+            print(wiadomosc2)
+            wagi_przesylek.append(sum(biezaca_przesylka))
+            biezaca_przesylka = []
+        if sum(biezaca_przesylka) > 20:
+            print(wiadomosc2)
+            biezaca_przesylka = biezaca_przesylka[:-1]
+            wagi_przesylek.append(sum(biezaca_przesylka))
+            biezaca_przesylka = []
+            biezaca_przesylka.append(waga_przedmiotu)
+
+
 waga_ost_paczki = sum(biezaca_przesylka)
 
 if waga_ost_paczki >= 1:
@@ -52,11 +70,18 @@ if waga_ost_paczki >= 1:
 for waga in wagi_przesylek:
     puste_kg.append(20 - waga)
 
+
 # indexing in Python starts from 0
 maks_puste_kg = max(puste_kg)
 index = (puste_kg.index(maks_puste_kg) + 1)
 
-print(f"Wysłano {len(lista)} przedmioty(ów) o wagach {lista} w {len(wagi_przesylek)}"
+if sum(lista) == 0:
+    lista = []
+
+
+# printing final summary
+print(f"\n----------\n"
+      f"Wysłano {len(lista)} przedmioty(ów) o wagach {lista} w {len(wagi_przesylek)}"
       f" paczkach. \nŁącznie wysłano {sum(lista)} kg.")
 
 if sum(puste_kg) > 0:
